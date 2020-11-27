91902ea98872e644962225cc8d033347234487e5
SuhaibKhawaja <suhaibkhawaja2000@gmail.com>
2020-11-27 22:39:03 +0500

    Task 2

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Text, View, StyleSheet, TextInput} from 'react-native';
+function App() {
+  const [ActualPrice, SetActualPrice] = useState(0);
+  const [Discount, SetDiscount] = useState(0);
 
-export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.textView}>
+      <Text style={styles.text}>Discount Calculator</Text>
+    </View>
+    <View>
+      <TextInput
+        style={styles.inputFields}
+        onChangeText={text => SetActualPrice(text)}
+        value={ActualPrice}
+        keyboardType="number-pad"
+        placeholder="Actual Price"
+      />
+      <TextInput
+        style={styles.inputFields}
+        onChangeText={text => SetDiscount(text)}
+        value={Discount}
+        keyboardType="number-pad"
+        placeholder="Discount Percentage"
+      />
+    </View>
+     
     </View>
   );
 }
@@ -14,8 +33,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+  inputFields:{
+    height: 40, borderColor: 'black', borderWidth: 1,
+    marginBottom:20,
+    padding:10,
+  },
+  textView:{
+    // borderWidth:1,
+    alignItems:'center',
+    padding:5,
+    marginBottom:15,
+  },
+  text:{
+    fontSize:20,
+    fontFamily:'monospace',
+    padding:10,
+  }
+
 });
+export default App;
